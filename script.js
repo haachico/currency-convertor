d1c515dd76bf98258f7760976ed7a7e4ff2a7b69
Nilesh <nilesh@haachico.com>
2022-12-26 17:03:37 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const typeCurrencyOne = document.querySelector(".currency-1-select");
+const typeCurrencyTwo = document.querySelector(".currency-2-select");
+const valueCurrencyOne = document.querySelector(".currency-1-values");
+const valueCurrencyTwo = document.querySelector(".curency-2-values");
+const exchangeRate = document.querySelector(".exchange-rate");
